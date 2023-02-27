9bef154efe3c4bd6014c9a7aa2d25c664cfa6552
Nguyễn Trọng Hoàng <116530644+space-hoang@users.noreply.github.com>
2023-02-28 00:26:43 +0700

    api

--- back-end/dist/src/constant/auction.constants.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AUCTION_STATUS = void 0;
-exports.AUCTION_STATUS = {
-    0: "Live",
-    1: "End",
-    2: "Time Auction",
-    3: "Open for offers",
-};
